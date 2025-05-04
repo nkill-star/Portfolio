635471e4126135a2af0f2d5e99f4cc2acd01689e
nikhil.s.richie@gmail.com <nikhil.s.richie@gmail.com>
2025-05-04 20:28:24 +0530

    updation

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,17 @@
+import '../components/style.css';
+
 export default function Projects() {
     return (
         <div className="container mx-auto border-b-2 border-green-950 py-5 ">
             <div className="flex justify-center">
                 <h2 className="text-white text-2xl italic font-bold [text-shadow:_0_4px_4px_rgb(0_255_0_/_0.5)]">MY CREATIONS</h2>
             </div>
-            <div className="flex overflow-x-auto scroll-smooth snap-x snap-mandatory space-x-10 px-10 py-5">
+            <div className="flex overflow-x-auto scroll-smooth snap-x snap-mandatory space-x-10 px-10 py-5 hide-scrollbar">
+            <div className="shrink-0 w-64 snap-start">
+                    <a href="https://github.com/nkill-star/Full-Stack-Netflix-Clone.git" target="_blank" rel="noopener noreferrer">
+                        <img src="netflix_fullstack.png" className="rounded-lg w-full cursor-pointer transition-transform duration-300 hover:scale-110" />
+                    </a>
+                </div>
                 <div className="shrink-0 w-64 snap-start">
                     <a href="https://github.com/nkill-star/Xs-and-Os-game.git" target="_blank" rel="noopener noreferrer">
                         <img src="game.png" className="rounded-lg w-full cursor-pointer transition-transform duration-300 hover:scale-110" />
